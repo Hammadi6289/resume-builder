c86699329df31b46fb478f84a22ce3ad6dfa06b5
Hammad Chohan <Hammad6289@email.com>
2025-12-11 15:47:18 +0500

    fixed the licide-react version... Although It still wont work.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Dashboard = () => {
+  //1:38:35
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 py-8">
@@ -10,8 +11,35 @@ const Dashboard = () => {
         >
           Welcome dummy user, "John Doe"
         </p>
-        <div className="flex gap-4">
-          <button></button>
+        <div className="flex gap-4 justify-center">
+          <button
+            className="w-full bg-white sm:max-w-48 h-48 flex flex-col items-center
+          justify-center rounded-lg gap-2 text-slate-600 border border-dashed
+          border-slate-300 group hover:border-green-500 hover:shadow-lg transition-all
+          duration-300 cursor-pointer"
+          >
+            <p
+              className="text-sm group-hover:text-green-600 transition-all
+            duration-300"
+            >
+              {" "}
+              Create Resume
+            </p>
+          </button>
+
+          <button
+            className="w-full bg-white sm:max-w-48 h-48 flex flex-col items-center
+          justify-center rounded-lg gap-2 text-slate-600 border border-dashed
+          border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all
+          duration-300 cursor-pointer"
+          >
+            <p
+              className="text-sm group-hover:text-purple-600 transition-all
+            duration-300"
+            >
+              Upload Existing Resume
+            </p>
+          </button>
         </div>
       </div>
     </div>
