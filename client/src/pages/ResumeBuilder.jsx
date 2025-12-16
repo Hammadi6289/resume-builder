d5175eb5f74da92314094de54310c008d4aa5370
Hammad Chohan <Hammad6289@email.com>
2025-12-16 12:03:19 +0500

    working on the Navigation Section of the main builder page.

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { dummyResumeData } from "../assets/assets";
-import { ArrowLeftIcon } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  Briefcase,
+  FileText,
+  FolderIcon,
+  GraduationCap,
+  Sparkles,
+  User,
+} from "lucide-react";
 
+/////2:31:31
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
   const [resumeData, setResumeData] = useState({
@@ -27,6 +36,20 @@ const ResumeBuilder = () => {
     }
   };
 
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+
+  const sections = [
+    { id: "personal", name: "personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ];
+
+  const activeSection = sections[activeSectionIndex];
+
   useEffect(() => {
     loadExistingResume();
   }, []);
@@ -46,7 +69,34 @@ const ResumeBuilder = () => {
       <div className="max-w-7xl mx-auto px-4 pb-8">
         <div className="grid lg:grid-cols-12 gap-8">
           {/* Left Panel form*/}
-          <div></div>
+          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+              {/* progress bar */}
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-green-400 border-none transition-all duration-2000"
+                style={{
+                  width: `${(activeSectionIndex * 100) / (sections.length - 1)}
+                %`,
+                }}
+              />
+
+              {/* Navigation Section*/}
+              <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+                <div></div>
+                <div className="flex items-center">
+                  {activeSectionIndex !== 0 && (
+                    <button
+                      className="flex items-center gap-1 p-3 rounded-lg
+                    text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all"
+                    ></button>
+                  )}
+                </div>
+              </div>
+              {/* End of the Navigation Section*/}
+            </div>
+          </div>
+          {/* End of the Left Panel form*/}
           {/* Right Panel form*/}
           <div></div>
         </div>
